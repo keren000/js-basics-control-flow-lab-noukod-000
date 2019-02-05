3f672ca12189d9da55414c63d5981f7a319f8ec9
Keren-Happuc Fleurimond <keren@noukod.com>
2019-02-05 19:33:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function switchOnCharmFromTip(tip) {
     case 'generous':
       result = 'Thank you so much.';
       break;
-    case 'Not as generous':
+    case 'not as generous':
       result = 'Thank you.';
       break;
     default:
